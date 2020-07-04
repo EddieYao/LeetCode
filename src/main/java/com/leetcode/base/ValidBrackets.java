66b74a40be26704f82eb891dc92a9376e4c7e5cd
EddieYao <2683361531@qq.com>
2020-07-04 10:39:34 +0800

    git init

---
+++ src/main/java/com/leetcode/base/ValidBrackets.java
@@ -0,0 +1,11 @@
+package com.leetcode.base;
+
+public class ValidBrackets {
+
+    public int longestValidParentheses(String s) {
+        String validBra = "()";
+        int length = s.length();
+        return 0;
+    }
+
+}
